84a3cb6560434f392417653348b0574480921655
Manpreet Kunnath <manpreetkunnath@gmail.com>
2025-06-26 21:48:47 +0530

    Update next.config.ts by adding asset prefix to bits

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 	// basePath: '/bits',
-	// assetPrefix: '/bits',
+	assetPrefix: '/bits',
 };
 
 export default nextConfig;
