6175d6ee148de283e9b100f8a92dd3b8884d1519
Manpreet Kunnath <manpreetkunnath@gmail.com>
2025-06-26 20:37:29 +0530

    Update next.config.ts with base path of bits

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+	basePath: '/bits',
+	assetPrefix: '/bits',
 };
 
 export default nextConfig;
